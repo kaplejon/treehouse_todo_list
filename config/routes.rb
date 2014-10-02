740a5f1459becf935a8520b49d52893904f9ede9
Lee Tratnyek <lee.tratnyek@me.com>
2014-10-02 14:28:55 -0400

    Added bcrypt gems, began session authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Odot::Application.routes.draw do
+  get "user_sessions/new"
+  get "user_sessions/create"
+  resources :users
+
   resources :todo_lists do
     resources :todo_items do
       member do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe User do
+  let(:valid_attributes) {
+    {
+      first_name: "Jason",
+      last_name: "Seifer",
+      email: "jason@teamtreehouse.com",
+      password: "treehouse1234",
+      password_confirmation: "treehouse1234"
+    }
+  }
+
+  context "validations" do
+    let(:user) { User.new(valid_attributes) }
+
+    before do
+      User.create(valid_attributes)
+    end
+
+    it "requires an email" do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "JASON@TEAMTREEHOUSE.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires the email address to look like an email" do
+      user.email = "jason"
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe "#downcase_email" do
+    it "makes the email attribute lowercase" do
+      user = User.new(valid_attributes.merge(email: "JASON@TEAMTREEHOUSE.COM"))
+      #user.downcase_email
+      #expect(user.email).to eq("jason@teamtreehouse.com")
+      expect{ user.downcase_email }.to change{ user.email }.
+        from("JASON@TEAMTREEHOUSE.COM").
+        to("jason@teamtreehouse.com")
+    end
+
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "MIKE@TEAMTREEHOUSE.COM"
+      expect(user.save).to be_truthy
+      expect(user.email).to eq("mike@teamtreehouse.com")
+    end
+  end
+end
