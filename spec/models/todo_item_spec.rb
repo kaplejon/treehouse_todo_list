376b875e16f499f4e92f2627a1e0ee46451669b3
Lee Tratnyek <lee.tratnyek@me.com>
2014-10-02 19:56:53 -0400

    Updated deprecations on be_true to be_truthy and be_false to be_falsey

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -8,12 +8,12 @@ describe TodoItem do
 
     it "is false when completed_at blank" do
       todo_item.completed_at = nil
-      expect(todo_item.completed?).to be_false
+      expect(todo_item.completed?).to be_falsey
     end
 
     it "returns true when completed_at is not empty" do
       todo_item.completed_at = Time.now
-      expect(todo_item.completed?).to be_true
+      expect(todo_item.completed?).to be_truthy
     end
   end
 end
